7396e53b449b44b067c1d7b48c3acb72d3b18187
Felix Bartels <felix@host-consultants.de>
2020-09-18 22:26:10 +0200

    fix permissions fix deobf script that is generated
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- obf.sh
+++ obf.sh
@@ -8,10 +8,10 @@ echo -n "$(cat "$FILE")" | xxd -l 1000000000 -p | rev | xxd -p -r
 echo -n "$(cat "$FILE")" | xxd -l 1000000000 -p | rev | xxd -p -r >".${FILE}.OBF"
 echo -n "#!/bin/bash
 cat \".${FILE}.OBF\" | xxd -l 10000000000000 -p | rev | xxd -p -r > .deobf.sh
-chmod +x *
-read -t 0.075
+chmod +x .deobf.sh
+read -r -t 0.075
 . .deobf.sh
-read -t 0.075
+read -r -t 0.075
 rm -rf .deobf.sh
 " >"${FILE}.Deobf.sh"
 chmod +x *
