8b82e9cbe52e2dff47debc2c35f289e7ba4c7238
Felix Bartels <felix@host-consultants.de>
2020-09-18 22:19:23 +0200

    do not make all files in current dir executeble shellcheck
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- example.sh.Deobf.sh
+++ example.sh.Deobf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat "example.sh.OBF" | xxd -l 10000000000000 -p | rev | xxd -p -r >".deobf.sh"
-chmod +x *
-read -t 0.075
+chmod +x .deobf.sh
+read -r -t 0.075
 . .deobf.sh
-read -t 0.075
+read -r -t 0.075
 rm -rf .deobf.sh
